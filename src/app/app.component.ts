f98010ee1d787807b211a1873306a818bc0973f6
Marco Odaguiri <marco.odaguiri@hashtech.com.br>
2018-04-19 15:37:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  
+  currentLat: any;
+  currentLong: any;
+  /*
+  addMapsScript() {
+  if (!document.querySelectorAll(`[src="${googleMapsUrl}"]`).length) { 
+    document.body.appendChild(Object.assign(
+      document.createElement('script'), {
+        type: 'text/javascript',
+        src: googleMapsUrl,
+        onload: () => doMapInitLogic()
+      }));
+  } else {
+    this.doMapInitLogic();
+  }
+}
+   */
+  ngOnInit() {    
+    
+    var mapProp = {      
+      center: new google.maps.LatLng(0, 0),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };  
+    
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+    if (navigator.geolocation) {      
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;                                        
+        let location = new google.maps.LatLng(this.currentLat, this.currentLong);        
+        this.map.panTo(location);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
 }
